29f6bf86d95edeaac9a0c2eb0e99c31bcf60ff62
Felipe Carvalho <felipeajc@gmail.com>
2024-10-14 16:50:48 +0200

    Commit tag

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,6 @@ buildscript {
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://jitpack.io") }
     }
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
